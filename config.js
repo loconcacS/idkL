88faba0546f182310ecf10b7fbc336d8bd03dbd8
NotLegiitt <85382919+NotLegiitt@users.noreply.github.com>
2021-06-05 12:30:37 +0700

    Update config.js

--- config.js
+++ config.js
@@ -13,7 +13,7 @@ const CONFIG = {
     btnReply: 'Gửi cho anh <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    messDesc: 'Tối nay đi chơi nhé công chúa.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/NotLegiit
+    messLink: 'https://m.me/NotLegiit' //link mess của các bạn. VD: https://m.me/NotLegiit
 }
